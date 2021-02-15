b582087e780cb3605f5e58721315fcfe352af698
GianluDeveloper <github@gianlu.dev>
2021-02-15 16:10:39 +0100

    📞 Update REST API call

--- src/pages/Clienti.tsx
+++ src/pages/Clienti.tsx
@@ -1,10 +1,19 @@
+import {
+  faEdit,
+  faPlusCircle,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { Cliente } from "../model/Interfaces";
 
 export default class Clienti extends Component {
   state = { clienti: [] };
   async componentDidMount() {
-    const response = await fetch("http://localhost:8080/JavaBiblioteca/api");
+    const response = await fetch(
+      "http://localhost:8080/JavaBiblioteca/publicapi/Corsi/findAll"
+    );
     const jsonResponse = await response.json();
     const clienti = [];
     for (let cliente of jsonResponse) {
@@ -16,14 +25,69 @@ export default class Clienti extends Component {
   render() {
     return (
       <div>
-        {this.state.clienti.map((els: Cliente) => {
-          return (
-            <div key={els.idCliente}>
-              {els.idCliente} -&gt; {els.nome} -&gt; {els.cognome}
-            </div>
-          );
-        })}
+        <h2>
+          Lista Clienti{" "}
+          <Link to="insert">
+            <FontAwesomeIcon
+              icon={faPlusCircle}
+              className="fa addWallet"
+              style={{ color: "red" }}
+              fixedWidth
+            />
+          </Link>
+        </h2>
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <th>idCliente</th>
+                <th>nome</th>
+                <th>cognome</th>
+                <th>telefono</th>
+                <th>Modifica</th>
+                <th>Elimina</th>
+              </tr>
+              {this.state.clienti.map((c: Cliente) => {
+                return (
+                  <tr>
+                    <td>{c.idCliente}</td>
+                    <td>{c.nome}</td>
+                    <td>{c.cognome}</td>
+                    <td>{c.telefono}</td>
+                    <td>
+                      <Link to="edit">
+                        <FontAwesomeIcon
+                          icon={faEdit}
+                          className="fa"
+                          fixedWidth
+                        />
+                      </Link>
+                    </td>
+                    <td>
+                      <Link to="trash">
+                        <FontAwesomeIcon
+                          icon={faTrash}
+                          className="fa"
+                          fixedWidth
+                        />
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
+      // <div>
+      //   {this.state.clienti.map((els: Cliente) => {
+      //     return (
+      //       <div key={els.idCliente}>
+      //         {els.idCliente} -&gt; {els.nome} -&gt; {els.cognome}
+      //       </div>
+      //     );
+      //   })}
+      // </div>
     );
   }
 }
--- src/pages/Corsi.tsx
+++ src/pages/Corsi.tsx
@@ -2,24 +2,23 @@ import React, { Component } from "react";
 import { Corso } from "../model/Interfaces";
 
 export default class Corsi extends Component {
-  state: Corso;
-  constructor(i: any, y: any) {
-    super(i, y);
-    this.state = {} as Corso;
-  }
+  state: Array<Corso> = [];
 
   async componentDidMount() {
     const request = await fetch(
-      "http://localhost:8080/JavaBiblioteca/api?type=corsi"
+      "http://localhost:8080/JavaBiblioteca/publicapi/Corsi/findAll"
     );
-    const listaCorsi: Corso = await request.json();
+    const listaCorsi: Array<Corso> = await request.json();
     // const listaCorsi: [Corso] = await request.json();
-    const corso: Corso = listaCorsi;
-    this.setState(corso);
+    //const corso: Corso = listaCorsi;
+    this.setState(listaCorsi);
   }
   render() {
-    let test: Corso = this.state;
-    if (test.hasOwnProperty("dataInizio")) {
+    let lista: Array<Corso> = this.state;
+    console.log(1, lista);
+    if (lista.length > 0 || lista[0] !== undefined) {
+      const test: Corso = lista[0];
+      console.log("hey", test);
       const data: Date = new Date(test.dataInizio);
       console.log(Object.getPrototypeOf(data));
       console.log(data.getDate());
